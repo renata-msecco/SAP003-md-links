6a426abe0eeb2675aa9a786e6361022cf38e068e
Renata <renata.secco@gmail.com>
2019-11-28 14:10:58 -0300

    indexfuncionando

---
+++ cli.js
@@ -0,0 +1,7 @@
+#! /usr/bin/env node
+const mdLinks = require('./lib/index.js');
+
+const path = process.argv.slice(0).pop();
+mdLinks(path)
+.then(result => console.log(result))
+.catch(error => console.log (error));
