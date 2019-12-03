77ca06182a8f469aa051241d73ced3bc5797e1db
Renata <renata.secco@gmail.com>
2019-12-03 17:44:31 -0300

    testes funcionando

--- cli.js
+++ cli.js
@@ -1,7 +1,15 @@
 #! /usr/bin/env node
+const chalk = require('chalk');
 const mdLinks = require('./lib/index.js');
 
 const path = process.argv.slice(0).pop();
 mdLinks(path)
-.then(result => console.log(result))
-.catch(error => console.log (error));
+  .then((result) => {
+    // eslint-disable-next-line array-callback-return
+    result.map((res) => {
+      // eslint-disable-next-line no-console
+      console.log('\n', chalk.red(res.text.substring(0, 50)), '\n', res.href);
+    });
+  })
+  // eslint-disable-next-line no-console
+  .catch(error => console.log(error));
